681e3974fe018892c530c8f494fbbb2736fa30ad
iEscapedVim <mail>
2025-01-12 15:49:59 +0500

    init

---
+++ src/consts.ts
@@ -0,0 +1,13 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SHORT_TITLE = 'IEV';
+export const SITE_DESCRIPTION = 'Welcome to my portfolio';
+export const NAME = 'Yasir Rehman'
+export const ROLE = 'Web Developer'
+export const FULL_TITLE = 'iEscapedVim'
+export const FACEBOOK = 'https://www.facebook.com/iEscapedVim1'
+export const TWITTER = 'https://twitter.com/iEscapedVim'
+export const INSTAGRAM = 'https://www.instagram.com/iEscapedVim_'
+export const GITHUB = 'https://github.com/iEscapedVim'
+export const LINKEDIN = 'https://www.linkedin.com/in/iEscapedVim/'
---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	// add my own theme colors to daisyui
+	daisyui: {
+		themes: [
+		{mytheme: {
+			"primary": "#041e42",	
+			"secondary": "#9067c6",
+			"accent": "#c0c0c0",
+			"neutral": "#36454f",
+			"base-100": "#e6e6fa",
+			"info": "#38bdf8",
+			"success": "#4ade80",
+			"warning": "#fbbf24",
+			"error": "#f87171",
+		},},
+		],
+	},
+	plugins: [
+		require("daisyui"),
+	],
+}
